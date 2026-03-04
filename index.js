7b73cce2651c54875caf9501bbb045ac0c6e139e
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-03-04 16:22:28 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+
+const inputText =document.getElementById("inputText");
+document.getElementById("PlayBtn").addEventListener("click",() =>{
+    const getValue = inputText.value;
+    // pronounceWord(getValue);
+    pronounceWord(getValue, "female");
+})
+
+
+let voices = [];
+
+function loadVoices() {
+  voices = speechSynthesis.getVoices();
+  if (!voices.length) {
+    // try again if empty
+    setTimeout(loadVoices, 100);
+  }
+}
+
+speechSynthesis.onvoiceschanged = loadVoices;
+loadVoices();
+
+function pronounceWord(word, gender = "female") {
+    if (!word) return; // empty string check
+  const utterance = new SpeechSynthesisUtterance(word);
+
+  let selectedVoice;
+
+  if (gender === "male") {
+    selectedVoice = getMaleVoice();
+  } else {
+    selectedVoice = getFemaleVoice();
+  }
+
+  if (selectedVoice) {
+    utterance.voice = selectedVoice;
+  }
+
+    utterance.lang = "bn-IN"; //"en-US"; American , "en-GB"; British , "en-AU" Australian
+  utterance.rate = 0.7;  // speed 0.7 >1.5
+  utterance.pitch = 2; // tone 0.5; Deep voice > 1.8; High voice
+  utterance.volume = 1; // volume 0.5; 50% volume > 1;  Full volume
+
+  window.speechSynthesis.speak(utterance);
+}
+
+function getFemaleVoice() {
+  return voices.find(v =>
+    v.name.toLowerCase().includes("zira") || //"zira" Windows female , "samantha" Mac female
+    v.name.toLowerCase().includes("female")
+  );
+}
+
+function getMaleVoice() {
+  return voices.find(v =>
+    v.name.toLowerCase().includes("david") ||
+    v.name.toLowerCase().includes("male")
+  );
+}
+
